6da22270da2a41faa0006f365243f3528e012e10
dearbear-zhang <dearbear_zhang@icloud>
2017-03-01 18:00:32 +0800

    //

---
+++ test/js/login.js
@@ -0,0 +1,21 @@
+/**
+ * Created by BYPC006 on 2017/3/1.
+ */
+function onLoginIn() {
+    var account = document.getElementById("account").value;
+    var psd = document.getElementById("pwd").value;
+    if (account.length == 0){
+        alert("请输入账号");
+        return
+    }
+    if (psd.length == 0){
+        alert("请输入密码")
+        return
+    }
+
+}
+
+
+
+
+
