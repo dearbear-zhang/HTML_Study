7a7f49f9d915b5ffac3169e98f390bb5fad02fc8
dearbear-zhang <dearbear_zhang@icloud>
2017-03-20 15:22:20 +0800

    //修改

--- test/js/my/home.js
+++ test/js/my/home.js
@@ -1,14 +1,16 @@
 /**
  * Created by BYPC006 on 2017/3/17.
  */
-function serach() {
-    var search = document.getElementById("search");
-    var searchInput = document.getElementById("search_input");
-    search.className = "be-box box-p-s";
-    searchInput.setAttribute("placeholder", "");
-}
-
 $(document).ready(function () {
+    search();
+    swiperInit();
+    sortRule()
+});
+
+/***
+ * 搜索框控制
+ */
+function search() {
     $("#search_input").focus(function () {
         // $(this).hide();
         $("#search").removeClass("box-p-c");
@@ -20,15 +22,31 @@ $(document).ready(function () {
         $("#search").addClass("box-p-c");
         $(this).attr("placeholder", "搜索商家,商品名称");
     })
+}
+
+/***
+ * 滑动控制
+ */
+function swiperInit() {
     var mySwiper = new Swiper('.swiper-container',{
         pagination : '.swiper-pagination',
         paginationClickable:true,
-        paginationElement:"li",
+        paginationElement:"li"
 //pagination : '#swiper-pagination1',
-    })
+    });
+}
 
+/***
+ * 商品排序规则
+ */
+function sortRule() {
     var sortItem = 0;
-    $("#item-text").onclick(function () {
+    $(".sortItem2").click(function () {
+        var itemIdx = $(this).attr("data-index");
+        // confirm(itemIdx)
+        $(".sortRule").find("li").removeClass("active");
+        $(".sortRule").find("img").attr("src", "images/coomon/head_jt02.png");
+        $(this).addClass("active");
+        $(this).find("img").attr("src", "images/coomon/head_jt01.png")
     })
-
-})
+}
